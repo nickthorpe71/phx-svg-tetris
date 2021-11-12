df00cafdf9d7061d7f7a197b6bd547b025ea1f1e
Nick Thorpe <nickthorpe71@gmail.com>
2021-11-12 09:07:34 -0600

    add shape pattern matching

--- lib/tetris_web/live/game_live.ex
+++ lib/tetris_web/live/game_live.ex
@@ -15,12 +15,12 @@ defmodule TetrisWeb.GameLive do
 
   def render(assigns) do
     ~L"""
-    <% [{x, y}] = @points %>
+    <% {x, y} = @tetro.location %>
     <section class="phx-hero">
       <h1> Welcome to Tetris</h1>
       <%= render_board(assigns) %>
       <pre>
-        {<%= x %>, <%= y %>}
+        {<%= inspect @tetro %>}
       </pre>
     </section>
     """
@@ -35,13 +35,15 @@ defmodule TetrisWeb.GameLive do
     """
   end
 
-  defp render_points(%{points: [{x, y}]} = assigns) do
+  defp render_points(assigns) do
     ~L"""
-    <rect
-      width="20" height="20"
-      x="<%= (x - 1) * 20 %>" y="<%= (y - 1) * 20 %>"
-      style="fill:rgb(9,62,0)stroke-width:2;stroke:rgb(45,122,0);"
-    />
+    <%= for {x,y} <- @points do %>
+      <rect
+        width="20" height="20"
+        x="<%= (x - 1) * 20 %>" y="<%= (y - 1) * 20 %>"
+        style="fill:rgb(9,62,0)stroke-width:2;stroke:rgb(45,122,0);"
+      />
+    <% end %>
     """
   end
 
@@ -51,7 +53,7 @@ defmodule TetrisWeb.GameLive do
 
   defp show(socket) do
     assign(socket,
-      points: Tetromino.points(socket.assigns.tetro)
+      points: Tetromino.show(socket.assigns.tetro)
     )
   end
 
