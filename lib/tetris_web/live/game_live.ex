2c5f92379b158d1f3d5fa63251f28c8e92cc4e81
Nick Thorpe <nickthorpe71@gmail.com>
2021-11-12 10:52:12 -0600

    add different colors for each shape

--- lib/tetris_web/live/game_live.ex
+++ lib/tetris_web/live/game_live.ex
@@ -3,7 +3,9 @@ defmodule TetrisWeb.GameLive do
   alias Tetris.Tetromino
 
   def mount(_params, _session, socket) do
-    :timer.send_interval(500, :tick)
+    if connected?(socket) do
+      :timer.send_interval(500, :tick)
+    end
 
     {
       :ok,
@@ -37,15 +39,24 @@ defmodule TetrisWeb.GameLive do
 
   defp render_points(assigns) do
     ~L"""
-    <%= for {x,y} <- @points do %>
+    <%= for {x, y, shape} <- @points do %>
       <rect
         width="20" height="20"
         x="<%= (x - 1) * 20 %>" y="<%= (y - 1) * 20 %>"
-        style="fill:rgb(9,62,0)stroke-width:2;stroke:rgb(45,122,0);"
+        style="fill:#<%=color(shape)%>;stroke-width:2;stroke:rgb(45,122,0);"
       />
     <% end %>
     """
   end
+
+  defp color(:l), do: "00237C"
+  defp color(:j), do: "300092"
+  defp color(:s), do: "600035"
+  defp color(:z), do: "461800"
+  defp color(:t), do: "272D00"
+  defp color(:o), do: "003545"
+  defp color(:i), do: "585858"
+  defp color(_), do: "093E00"
 
   defp new_tetromino(socket) do
     assign(socket, tetro: Tetromino.new_random())
