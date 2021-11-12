5710e8fc0929f51cb74f1685c089388d2a02d73b
Nick Thorpe <nickthorpe71@gmail.com>
2021-11-12 13:47:59 -0600

    add bounderies to front end

--- lib/tetris/game.ex
+++ lib/tetris/game.ex
@@ -1,5 +1,5 @@
 defmodule Tetris.Game do
-  defstruct [:tetro, score: 0, junkyard: %{}]
+  defstruct [:tetro, points: [], score: 0, junkyard: %{}]
   alias Tetris.{Tetromino, Points}
 
   def move(game, move_fn) do
@@ -7,5 +7,18 @@ defmodule Tetris.Game do
     new = game.tetro |> move_fn.()
     valid = new |> Tetromino.show() |> Points.valid?()
     Tetromino.maybe_move(old, new, valid)
+  end
+
+  def left(game), do: game |> move(&Tetromino.left/1)
+  def right(game), do: game |> move(&Tetromino.right/1)
+  def rotate(game), do: game |> move(&Tetromino.rotate/1)
+
+  def new_tetromino(game) do
+    %{game | tetro: Tetromino.new_random()}
+    |> show
+  end
+
+  def show(game) do
+    %{game | points: Tetromino.show(game.tetro)}
   end
 end
